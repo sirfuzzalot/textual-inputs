fff3eaa1a452b8324551a2f78e17cd394d88d1b7
Daniel Mouritzen <dmrtzn@gmail.com>
2021-10-17 13:53:09 +0200

    Allow unicode text input

--- src/textual_inputs/text_input.py
+++ src/textual_inputs/text_input.py
@@ -240,7 +240,7 @@ class TextInput(Widget):
                     )
             await self._emit_on_change(event)
 
-        elif event.key in string.printable:
+        elif len(event.key) == 1 and event.key.isprintable():
             if self._cursor_position == 0:
                 self.value = event.key + self.value
             elif self._cursor_position == len(self.value):
